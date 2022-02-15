48e106f44497be6385e67427e4da64d38bb41932
hend <HindMahmoud@users.noreply.github.com>
2022-02-15 00:45:04 -1000

    Add files via upload

--- Models/AccountViewModels.cs
+++ Models/AccountViewModels.cs
@@ -1,17 +1,16 @@
 ﻿using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using MedicalTansik.Models;
 
-namespace MedicalTansik.ViewModels
+namespace MedicalTansik.Models
 {
     public class ExternalLoginConfirmationViewModel
     {
         [Required]
         [Display(Name = "الرقم القومي")]
-        [MaxLength(14,ErrorMessage="الرقم القومي يجب ان يكون 14 رقم")]
-        public string NationalId { get; set; }
-        [Required]
-        [Display(Name = "كلمة المرور")]
-        public string password { get; set; }
+        [StringLength(14, ErrorMessage = "الرقم القومي يجب ان يكون 14 رقم", MinimumLength = 14)]
+        public string nationalId { get; set; }
+
     }
 
     public class ExternalLoginListViewModel
@@ -43,50 +42,72 @@ namespace MedicalTansik.ViewModels
     //    public bool RememberMe { get; set; }
     //}
 
+    //public class ForgotViewModel
+    //{
+    //    [Required]
+    //    [Display(Name = "Email")]
+    //    public string Email { get; set; }
+    //}
+
     public class LoginViewModel
     {
         [Required]
         [Display(Name = "الرقم القومي")]
         [MaxLength(14, ErrorMessage = "الرقم القومي يجب ان يكون 14 رقم")]
-        public string nationalId { get; set; }
-
+        public string NationalId { get; set; }
         [Required]
         [DataType(DataType.Password)]
         [Display(Name = "كلمة المرور")]
         public string Password { get; set; }
 
-        [Display(Name = "Remember me?")]
-        public bool RememberMe { get; set; }
+       
     }
 
-  
+    //public class RegisterViewModel
+    //{
+    //    [Required]
+    //    [EmailAddress]
+    //    [Display(Name = "Email")]
+    //    public string Email { get; set; }
 
-    public class ResetPasswordViewModel
-    {
-        [Required]
-        [EmailAddress]
-        [Display(Name = "Email")]
-        public string Email { get; set; }
+    //    [Required]
+    //    [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+    //    [DataType(DataType.Password)]
+    //    [Display(Name = "كلمة المرور ")]
+    //    public string Password { get; set; }
 
-        [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
-        [DataType(DataType.Password)]
-        [Display(Name = "Password")]
-        public string Password { get; set; }
+    //    [DataType(DataType.Password)]
+    //    [Display(Name = "Confirm password")]
+    //    [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+    //    public string ConfirmPassword { get; set; }
+    //}
 
-        [DataType(DataType.Password)]
-        [Display(Name = "Confirm password")]
-        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
-        public string ConfirmPassword { get; set; }
+    //public class ResetPasswordViewModel
+    //{
+    //    [Required]
+    //    [EmailAddress]
+    //    [Display(Name = "Email")]
+    //    public string Email { get; set; }
 
-        public string Code { get; set; }
-    }
+    //    [Required]
+    //    [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+    //    [DataType(DataType.Password)]
+    //    [Display(Name = "كلمة المرور ")]
+    //    public string Password { get; set; }
 
-    public class ForgotPasswordViewModel
-    {
-        [Required]
-        [EmailAddress]
-        [Display(Name = "Email")]
-        public string Email { get; set; }
-    }
+    //    [DataType(DataType.Password)]
+    //    [Display(Name = "Confirm password")]
+    //    [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+    //    public string ConfirmPassword { get; set; }
+
+    //    public string Code { get; set; }
+    //}
+
+    //public class ForgotPasswordViewModel
+    //{
+    //    [Required]
+    //    [EmailAddress]
+    //    [Display(Name = "Email")]
+    //    public string Email { get; set; }
+    //}
 }
